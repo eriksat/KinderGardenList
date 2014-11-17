f39eba699f52b33005b26ec8db359ec348f53313
adilet571 <adilet.joldoshbekov9@facebook.com>
2014-11-17 11:09:21 +0600

    Log in ADmin jana user kowtum

--- KinderGartenList/KinderGartenList/KinderGartenList/LogIn.Designer.cs
+++ KinderGartenList/KinderGartenList/KinderGartenList/LogIn.Designer.cs
@@ -28,20 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightGray;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.BorderSize = 2;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(349, 214);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 31);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "L&ogin";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.GhostWhite;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(393, 121);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(209, 32);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightGray;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button2.FlatAppearance.BorderSize = 2;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(491, 214);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 31);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "C&ancel";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Lavender;
+            this.label1.Location = new System.Drawing.Point(243, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "LOGIN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Lavender;
+            this.label2.Location = new System.Drawing.Point(243, 121);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 26);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "PASSWORD";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.GhostWhite;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Admin",
+            "User"});
+            this.comboBox1.Location = new System.Drawing.Point(393, 50);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(209, 33);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.TextChanged += new System.EventHandler(this.comboBox1_TextChanged);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
             // 
             // LogIn
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(590, 429);
+            this.BackColor = System.Drawing.Color.Navy;
+            this.ClientSize = new System.Drawing.Size(659, 309);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "LogIn";
             this.Text = "LogIn";
             this.Load += new System.EventHandler(this.LogIn_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
     }
 }
--- KinderGartenList/KinderGartenList/KinderGartenList/LogIn.cs
+++ KinderGartenList/KinderGartenList/KinderGartenList/LogIn.cs
@@ -2,16 +2,23 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 namespace KinderGartenList
 {
     public partial class LogIn : Form
     {
+        private SqlConnection con;
+        private SqlQuery1 ob;
+        private SqlDataAdapter cmd;
+        private DataTable dt;
+        private string userLogin;
+        private string userPassword;
+        private string query;
         public LogIn()
         {
             InitializeComponent();
@@ -19,7 +26,93 @@ namespace KinderGartenList
 
         private void LogIn_Load(object sender, EventArgs e)
         {
+            con = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=D:\DOCUMENTS\project C#\MyfirstProject GitHub\KinderGardenList\KinderGartenList\KinderGartenList\KinderGartenList\inform.mdf;Integrated Security=True");
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
 
         }
+
+        private void SetUser()
+        {
+            userLogin= comboBox1.Text;
+            userPassword = userLogin + "Password";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SetUser();
+            if (comboBox1.Text == "" && textBox2.Text=="") {
+                errorProvider1.SetError(comboBox1, "Please Choose Users!");
+                errorProvider1.SetError(textBox2,"Please Fill Password!");
+            }
+            else if (comboBox1.Text != "" && textBox2.Text == "")
+            {
+                errorProvider1.SetError(textBox2, "Please Fill Password!");
+            }
+            else if (comboBox1.Text == "" && textBox2.Text != "")
+            {
+                errorProvider1.SetError(comboBox1, "Please Choose Users!"); 
+            }
+            else
+            {
+               
+                try
+                {
+                    query = "Select * from Users Where " + userLogin + "='" + comboBox1.Text + "' and " + userPassword +
+                            "='" + textBox2.Text + "'";
+                    if(con==null)
+                    con.Open();
+                    cmd = new SqlDataAdapter(query,con);
+                    dt  = new DataTable();
+                    cmd.Fill(dt);
+                    if (dt.Rows[0][0].ToString() == "1")
+                    {
+                     con.Close();
+                        if (userLogin == "Admin")
+                        {
+                            this.Hide();
+                            AdminMainForm frAdmin = new AdminMainForm();
+                            frAdmin.Show();
+                        }
+                        else //if(userLogin   == "User")
+                        {
+                            this.Hide();
+                         
+                            UserMainForm frUser = new UserMainForm();
+                                frUser.Show();
+                            
+                        }
+
+                    }
+
+                    else
+                    {
+                        MessageBox.Show("LOGIN OR PASSWORD INCORRECT!");
+                    }
+
+
+                }
+                catch (Exception kataException)
+                {
+                    MessageBox.Show(kataException.Message);
+                 
+                }
+            }
+        }
+
+        private void comboBox1_TextChanged(object sender, EventArgs e)
+        {
+            errorProvider1.SetError(comboBox1,"");
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            errorProvider1.SetError(textBox2,"");
+        }
+
+      
     }
 }
